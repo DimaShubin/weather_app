ddd8f93fd0cc88ca77e8e2372a17b39ce37c45e4
Dmitry Shubin <20dimaha@gmail.com>
2019-11-10 17:44:42 +0200

    Updates

---
+++ precache-manifest.ad36aeb8d719f9b3c8d0ac8cf6cbae5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c94f629b42b412c0cf36f74feeac30",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "a50524a41141eaa9d1d6",
+    "url": "/weather_app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "157d03c93d579a727105",
+    "url": "/weather_app/static/js/2.e2bc3169.chunk.js"
+  },
+  {
+    "revision": "a50524a41141eaa9d1d6",
+    "url": "/weather_app/static/js/main.3bbfad52.chunk.js"
+  },
+  {
+    "revision": "e0404d2bc9c0fec915b5",
+    "url": "/weather_app/static/js/runtime-main.f44fb299.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/weather_app/static/media/logo.25bf045c.svg"
+  }
+]);
